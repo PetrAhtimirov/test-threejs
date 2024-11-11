103e27076a1414f6b2a3575c472a6fc30597dd62
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-11-11 10:47:48 +0300

    fix paths

--- objects/temple.js
+++ objects/temple.js
@@ -2,15 +2,19 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 import { TextureLoader, DoubleSide } from 'three';
 import {scene} from "../main.js";
+import mtlFilePath from '../models/japanese_temple/Japanese_Temple.mtl';
+import textureFilePath from '../models/japanese_temple/Japanese_Temple_Paint2_Japanese_Shrine_Mat_AlbedoTransparency.png';
+import objFilePath from '../models/japanese_temple/Japanese_Temple.obj'
+
 
 export const initTemple = () => {
     // Создаем загрузчик текстуры
     const textureLoader = new TextureLoader();
-    const texture = textureLoader.load('models/japanese_temple/Japanese_Temple_Paint2_Japanese_Shrine_Mat_AlbedoTransparency.png'); // Укажите путь к текстуре
+    const texture = textureLoader.load(textureFilePath); // Укажите путь к текстуре
 
     // Загрузка материалов из .mtl файла
     const mtlLoader = new MTLLoader();
-    mtlLoader.load('models/japanese_temple/Japanese_Temple.mtl', function (materials) {
+    mtlLoader.load(mtlFilePath, function (materials) {
         materials.preload();
 
         // Устанавливаем текстуру для каждого материала
@@ -24,7 +28,7 @@ export const initTemple = () => {
         // Загрузка объекта из .obj файла с примененными материалами
         const objLoader = new OBJLoader();
         objLoader.setMaterials(materials);
-        objLoader.load('models/japanese_temple/Japanese_Temple.obj', function (object) {
+        objLoader.load(objFilePath, function (object) {
             const scale = 1.2;
             object.scale.set(scale, scale, scale)
             object.traverse(function (child) {
